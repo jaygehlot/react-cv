e6e7aaef0948c757bd2331ae7770cb285c6d231b
Jay Gehlot <jay.gehlot2@vodafone.com>
2020-11-15 16:12:26 +0000

    Updates

---
+++ precache-manifest.259cc4ec9103a9662cc2f33e03f132b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2088841d133fc43fa8a9faaffe246d6c",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "fcb64feeead911a0ed80",
+    "url": "/react-cv/static/js/2.d819365a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-cv/static/js/2.d819365a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37b5ac26086f73e22ad",
+    "url": "/react-cv/static/js/main.8acc45ba.chunk.js"
+  },
+  {
+    "revision": "5a388952eaca470c8b93",
+    "url": "/react-cv/static/js/runtime-main.a1626795.js"
+  }
+]);
